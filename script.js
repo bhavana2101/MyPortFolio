12eef43e57cde661b53a90010b35864b5ebd8dd7
bhavana2101 <140957992+bhavana2101@users.noreply.github.com>
2024-02-22 16:53:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollHeight = Math.round((pos*100)/calcHeight);
+    if (pos > 100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
